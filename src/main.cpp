c2751facd144ff0f6b60568009e5a72273fe4d4d
Marco Mora <morafoo@gmail.com>
2024-03-28 20:10:22 -0600

    More cleanup
    Signed-off-by: Marco Mora <morafoo@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,17 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #if defined(ESP8266)
-  #include <ESP8266WiFi.h>  //ESP8266 Core WiFi Library         
+#include <ESP8266WiFi.h>  //ESP8266 Core WiFi Library
 #else
-  #include <WiFi.h>         //ESP32 Core WiFi Library    
+#include <WiFi.h>  //ESP32 Core WiFi Library
 #endif
 #if defined(ESP8266)
-  #include <ESP8266WebServer.h>  //Local WebServer used to serve the configuration portal
+#include <ESP8266WebServer.h>  //Local WebServer used to serve the configuration portal
 #else
-  #include <WebServer.h>         //Local DNS Server used for redirecting all requests to the configuration portal 
+#include <WebServer.h>  //Local DNS Server used for redirecting all requests to the configuration portal
 #endif
 
-#include <DNSServer.h> //Local WebServer used to serve the configuration portal 
+#include <DNSServer.h>  //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>
 
 #include "garden.h"
@@ -69,6 +69,42 @@ byte pump_on[] = {0x04, 0x0E, 0x15, 0x04, 0x04, 0x04, 0x04, 0x04};
 
 byte pump_off[] = {0x00, 0x00, 0x11, 0x0A, 0x04, 0x0A, 0x11, 0x00};
 
+// Initialize the display and create the characters to print
+void init_display() {
+  // Wire.begin();
+  // scan_i2c();
+  // Liquid Crystal
+  lcd.init();
+  lcd.backlight();
+  lcd.createChar(0, pump_on);
+  lcd.createChar(1, pump_off);
+}
+
+void inint_timers() {
+  // number of timer, prescalar value, true count up
+  timer1 = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer1, &check_status, true);
+  //   1000000 = 1 seg - units are microseconds
+  // 600000000 = 10 min
+  timerAlarmWrite(timer1, 1000000, true);
+  timerAlarmEnable(timer1);
+}
+
+void init_wifi() {
+  // Create a wifiManager instance
+  WiFiManager wm;
+  // Remove comments to reset configuration
+  // wifiManager.resetSettings();
+
+  // Create AP portal and check if connected
+  if (!wm.autoConnect("ESPRiego")) {
+    Serial.println("Fail connecting (timeout)");
+    ESP.restart();
+    delay(1000);
+  }
+  Serial.println("Connection succesful");
+}
+
 void print_checking() {
   lcd.clear();
   lcd.setCursor(2, 1);
@@ -105,46 +141,33 @@ void print_no_water() {
   lcd.print("** WATER **");
 };
 
-void setup() {
-  Serial.begin(115200);
-  // number of timer, prescalar value, true count up
-  timer1 = timerBegin(0, 80, true);
-  timerAttachInterrupt(timer1, &check_status, true);
-  //   1000000 = 1 seg - units are microseconds
-  // 600000000 = 10 min
-  timerAlarmWrite(timer1, 1000000, true);
-  timerAlarmEnable(timer1);
-
-  disable_system = false;
-  // Set the pin where the water container sensor will be connected
-  pinMode(WATER_SENSOR_PIN, INPUT_PULLUP);
-
-  // Create a wifiManager instance
-  WiFiManager wm;
-  // Remove comments to reset configuration
-  //wifiManager.resetSettings();
- 
-  // Create AP portal and check if connected
-  if(!wm.autoConnect("ESPRiego")){
-    Serial.println("Fail connecting (timeout)");
-    ESP.restart();
-    delay(1000);
-  }
-  Serial.println("Connection succesful");
-
+void init_garden() {
   // Analog pin, digital pin to relay, humidity threshold, seconds on
   garden.addPlant(plant1, A0, RELAY1, 30, 10);
   garden.addPlant(plant2, A3, RELAY2, 30, 10);
   garden.addPlant(plant3, A7, RELAY3, 30, 10);
   garden.addPlant(plant4, A19, RELAY4, 30, 10);
+}
 
-  // Wire.begin();
-  // scan_i2c();
-  // Liquid Crystal
-  lcd.init();
-  lcd.backlight();
-  lcd.createChar(0, pump_on);
-  lcd.createChar(1, pump_off);
+void init_gpio() {
+  disable_system = false;
+  // Set the pin where the water container sensor will be connected
+  pinMode(WATER_SENSOR_PIN, INPUT_PULLUP);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  inint_timers();
+
+  init_gpio();
+
+  init_wifi();
+
+  init_garden();
+
+  init_display();
+
   check_status();
 }
 
@@ -152,10 +175,11 @@ void loop() {
   if ((min10 >= CHECK_PERIOD) && !disable_system) {
     min10 = 0;
     Serial.println("Check plants");
-    // check humidity and turn pump if required.
     print_checking();
+    // check humidity and turn pump if required.
     garden.checkPlants();
   }
+
   if (disable_system) {
     print_no_water();
   } else {
